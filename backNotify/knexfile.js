c60713f34b62a2e879d994760441f7ec1e189b37
DaviSpin <davi@3v3.com.br>
2020-10-28 10:28:08 -0300

    V5

--- backNotify/knexfile.js
+++ backNotify/knexfile.js
@@ -34,8 +34,8 @@ module.exports = {
   connection: {
     host: "localhost",
     user: "root",
-    password: "123v312",
-    database: "newsir",
+    password: "3v3",
+    database: "NOTIFY",
   },
   migrations: {
     directory: path.resolve(__dirname, "src", "database", "migrations"),
