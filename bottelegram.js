4e37e60f55cf9aedbea4e880490fcb70cc756fda
Naylton Nobre <65734371+NayltonNobre-3v3@users.noreply.github.com>
2021-11-18 15:35:06 -0300

    Melhorias bot telegram

--- bottelegram.js
+++ bottelegram.js
@@ -16,18 +16,32 @@ const envtelegram = new cron.schedule('*/5 * * * *', () => {
   let data = new Date();
   let hora = data.getHours();
   let min = data.getMinutes();
-  let seg = data.getSeconds();
-  let hourcomplete = hora + ':' + min + ':' + seg;
+  let hourcomplete = hora + ':' + min;
 //Variáveis responsáveis por retornar a data e hora atual.
-  bot.telegram.sendMessage(CHAT_ID, 'Varredura executada as '
-  + hourcomplete)});
+  bot.telegram.sendMessage(CHAT_ID, 
+    '\u{1F501}' + 'Verificação executada as ' 
+    + hourcomplete  + '. ' + '\u{1F55C}'
+    + '\nhá [X] Dispositivos com problema' + ', '
+    + '\nhá [X] Atuadores com problema' + ', ' 
+    + '\nhá [X] Sensores com problema, \n\nSegue abaixo suas especificações:' + ' \u{1F4AC}'
+    + ' \n\n\u{1F4F6}' + 'Dispositivos:' 
+    + '\nXXXXXXXXXXXXXXXX'
+    + '\nXXXXXXXXXXXXXXXX'
+    + ' \n\n\u{1F4A7}' + 'Atuadores:'    
+    + '\nXXXXXXXXXXXXXXXX'
+    + '\nXXXXXXXXXXXXXXXX'
+    + ' \n\n\u{1F321}' + 'Sensores:'    
+    + '\nXXXXXXXXXXXXXXXX'
+    + '\nXXXXXXXXXXXXXXXX'
+    )});
 //função responsável pela mensagem da varredura dos alertas.
 const envtelegram2 = new cron.schedule('0 7 * * *', () =>{
   bot.telegram.sendMessage(CHAT_ID, 'Bom dia a todos!')
+  
 });
 //teste - função responsável por ser educado part1
 
-const envtelegram3 = new cron.schedule('0 12 * * *', () => {
+const envtelegram3 = new cron.schedule('1 13 * * *', () => {
   bot.telegram.sendMessage(CHAT_ID, 'Boa tarde a todos!')
 })
 //teste - função responsável por ser educado part2
@@ -39,5 +53,3 @@ const envtelegram4 = new cron.schedule('0 18 * * *', () => {
 
   bot.launch();
 
-
-  
